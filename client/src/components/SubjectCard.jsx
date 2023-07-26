facef433e0808963be155f767bffdf32c8ea143b
thanhdxuan <hello.thanhdauxuan@gmail.com>
2023-07-27 00:00:53 +0700

    add img, title

--- client/src/components/SubjectCard.jsx
+++ client/src/components/SubjectCard.jsx
@@ -1,18 +1,44 @@
 import { Box, useTheme, Paper, Grid} from '@mui/material';
+import { red } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
 import { tokens } from '../theme';
 
 const SubjectCard = ({cardInfo}) => {
    const theme = useTheme();
    const colors = tokens(theme.palette.mode);
    return (
-      <Grid>
-         <Paper
-          sx={{
-            height: 140,
-            width: 280,
-            backgroundColor: colors.blackAccent[700]
-         }} />
-      </Grid>
+      <Box
+         display="flex"
+         backgroundColor={colors.grayAccent[300]}
+         borderRadius={4}
+         sx= {{
+            '&:hover': {
+               borderBottom: 2,
+               borderBottomColor: colors.grayAccent[600],
+               cursor: "pointer"
+            }
+         }}
+      >
+         <Box sx={{ mx: 1, my: 1 }} width = "20%">
+            <img
+               src="/images/animals_1.svg"
+               style={{
+                  borderRadius: 15,
+                  display: 'block',
+                  width: '100%',
+              }}
+               loading="lazy"
+            />
+         </Box>
+         <Box sx={{ my: 2, ml: 2 }}>
+            <Typography
+               variant='h5'
+               fontWeight="bold"
+            >
+               Chemistry: The Central Science
+            </Typography>
+         </Box>
+      </Box>
    );
 }
 
