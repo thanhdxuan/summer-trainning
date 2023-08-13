98dedcc1050d4529c640223d4d5c8d5c6fba8934
thanhdxuan <hello.thanhdauxuan@gmail.com>
2023-08-13 23:26:27 +0700

    client: refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { ColorModeContext, useMode } from './theme.js';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider, Divider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './sences/global/TopBar.jsx';
 import Home from './sences/home'
@@ -8,18 +9,19 @@ import PostsPage from './sences/posts'
 const App = () => {
   const [theme, colorMode] = useMode();
   return (
-    <ColorModeContext.Provider value={ colorMode }>
-      <ThemeProvider theme={ theme }>
-        <CssBaseline/>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <div className='app'>
           <main className='content'>
-            <Topbar/>
+            <Topbar />
+            <Divider flexItem />
             <Routes>
-              <Route path="/" element= { <Home /> } />
-              <Route path="/topics/:topicId/posts" element= { <PostsPage /> } />
+              <Route path="/" element={<Home />} />
+              <Route path="/topics/:topicId/posts" element={<PostsPage />} />
             </Routes>
           </main>
-        </div> 
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
