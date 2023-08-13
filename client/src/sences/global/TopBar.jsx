da090db68e5e607b043cc18af2fa96fe7c612544
thanhdxuan <hello.thanhdauxuan@gmail.com>
2023-08-14 03:04:44 +0700

    client: add admin page

--- client/src/sences/global/TopBar.jsx
+++ client/src/sences/global/TopBar.jsx
@@ -1,12 +1,13 @@
-import { Box, IconButton, useTheme} from '@mui/material';
+import { Box, Container, IconButton, useTheme } from '@mui/material';
 import { useContext } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
-
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
-import PersionOutlinedIcon from '@mui/icons-material/PersonOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersionOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import { Link } from 'react-router-dom';
+
 
 
 const Topbar = () => {
@@ -15,34 +16,37 @@ const Topbar = () => {
    const colorMode = useContext(ColorModeContext);
 
    return (
-      <Box display="flex" justifyContent="space-between" p={2} alignItems="center">
-         {/* ICONS */}
-         <Box display="flex"> <IconButton onClick={colorMode.toggleColorMode}>
-               {theme.palette.mode === 'dark' ? (
-                  <DarkModeOutlinedIcon></DarkModeOutlinedIcon>
-               ) : (
-                  <LightModeOutlinedIcon></LightModeOutlinedIcon>
-               )}
-            </IconButton>
-            <IconButton>
-               <NotificationsOutlinedIcon />
-            </IconButton>
-            <IconButton>
-               <PersionOutlinedIcon />
-            </IconButton>
-            <IconButton>
-               <SettingsOutlinedIcon />
-            </IconButton>
-         </Box>
-         <Box display="flex">
-            <img
-               src='/images/logo/light-removebg.png'
-               style={{
-                  height: 40
-               }}
-            />
-         </Box>
-      </Box>
+      <Container>
+         <Box display="flex" justifyContent="space-between" py={2} alignItems="center">
+            {/* ICONS */}
+            <Box display="flex">
+               <img
+                  src='/images/logo/light-removebg.png'
+                  style={{
+                     height: 40
+                  }}
+               />
+            </Box>
+            <Box display="flex" alignItems="center">
+               <IconButton onClick={colorMode.toggleColorMode}>
+                  {theme.palette.mode === 'dark' ? (
+                     <DarkModeOutlinedIcon></DarkModeOutlinedIcon>
+                  ) : (
+                     <LightModeOutlinedIcon></LightModeOutlinedIcon>
+                  )}
+               </IconButton>
+               <IconButton component={Link} to="/admin">
+                  <AdminPanelSettingsOutlinedIcon />
+               </IconButton>
+               <IconButton>
+                  <PersionOutlinedIcon />
+               </IconButton>
+               <IconButton>
+                  <SettingsOutlinedIcon />
+               </IconButton>
+            </Box>
+         </Box >
+      </Container>
    );
 
 }
